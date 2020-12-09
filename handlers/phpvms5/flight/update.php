558fa2a904b591c2187f67324e42d771df1b1cd4
Collin Biedenkapp <cbiedenkapp@tfdidesign.com>
2020-12-09 13:48:44 -0500

    Updated update.php

--- handlers/phpvms5/flight/update.php
+++ handlers/phpvms5/flight/update.php
@@ -2,15 +2,54 @@
 if(!defined('API'))
     exit;
 
+errorOut(503, 'Work in progress');
+
 assertData(
-    array('latitude' => 'number', 'longitude' => 'number', 'magneticheading' => 'number', 'trueheading' => 'number', 'altitude' => 'number', 'groundspeed' => 'number', 'phase' => 'string', 'departuretime' => 'string', 'arrivaltime' => 'string', 'distanceremaining' => 'number', 'timeremaining' => 'string'),
+    array('instance' => 'number', 'latitude' => 'number', 'longitude' => 'number', 'magneticheading' => 'number', 'trueheading' => 'number', 'altitude' => 'number', 'groundspeed' => 'number', 'phase' => 'string', 'departuretime' => 'string', 'arrivaltime' => 'string', 'distanceremaining' => 'number', 'timeremaining' => 'string'),
     $_POST
 );
 
 $pilot = $database->fetch('SELECT * FROM ' . dbPrefix . 'pilots WHERE pilotid = ?', array($dbID));
 
 if(!empty($pilot))
-{
-
+{    
+    //WIP
+    $lat = str_replace(",", ".", $latitude);
+    $lon = str_replace(",", ".", $longitude);
+    
+    $lat = doubleval($lat);
+    $lon = doubleval($lon);
+    
+    if($lon < 0.005 && $lon > -0.005)
+        $lon = 0;
+        
+    if($lat < 0.005 && $lat > -0.005)
+        $lat = 0;        
+    
+    $fields = array(
+        'pilotid' =>$dbid,
+        'flightnum' =>$flightnumber,
+        'pilotname' => $pilot['firstname'] . " " . $pilot['lastname'],
+        'aircraft' =>$aircraft,
+        'lat' =>$lat,
+        'lng' =>$lon,
+        'heading' =>$magneticheading,
+        'alt' =>$altitude,
+        'gs' =>$groundspeed,
+        'depicao' =>$departureicao,
+        'arricao' =>$arrivalicao,
+        'deptime' =>$departuretime,
+        'arrtime' =>$arrivaltime,
+        'route' =>$route,
+        'distremain' =>$distanceremaining,
+        'timeremaining' =>$timeremaining,
+        'phasedetail' =>$phases[$phase],
+        'online' => $onlinenetwork,
+        'client' =>'smartCARS',
+    );
+    
+    return(ACARSData::UpdateFlightData($dbid, $fields));
 }
+else
+    errorOut(404, 'Pilot data not found');
 ?>
