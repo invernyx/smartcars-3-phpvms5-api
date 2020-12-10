29b014ab1e8762e9bce30283da03c263f2d1f0e4
GenericNerd <45597752+GenericNerd@users.noreply.github.com>
2020-12-10 18:05:53 +0000

    Completed charter flight creation

--- handlers/phpvms5/flight/create.php
+++ handlers/phpvms5/flight/create.php
@@ -1,7 +1,4 @@
 <?php
-// Create schedule into schedules table
-// Get ID and bid on it
-// Get Bid ID and Schedule ID to charteredFlights table
 if(!defined('API'))
     exit;
 
@@ -38,10 +35,50 @@ $params = array(
     $_POST['ticketPrice'],
     $type
 );
-$database->execute('INSERT INTO ' . dbPrefix . 'schedules (id, code, flightnum, depicao, arricao, route, aircraft, flightlevel, distance, deptime, arrtime, flighttime, price, flighttype, enabled) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)',$params);
+if ($database->execute('INSERT INTO ' . dbPrefix . 'schedules (id, code, flightnum, depicao, arricao, route, aircraft, flightlevel, distance, deptime, arrtime, flighttime, price, flighttype, enabled) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0)',$params) != true)
+    errorOut(500,'Unable to create schedules');
 $scheduleID = $database->getLastInsertID();
-$database->execute('INSERT INTO ' . dbPrefix . 'bids (bidid, pilotid, routeid, dateadded) VALUES (NULL, ?, ?, NOW())',array($dbID, $scheduleID));
+$pilotName = $database->fetch('SELECT firstname, lastname FROM ' . dbPrefix . 'pilots WHERE pilotid=?',array($dbID));
+if ($pilotName == array())
+    errorOut(404,'Pilot not found');
+$pilotName = $pilotName[0];
+$departureAirport = $database->fetch('SELECT name FROM ' . dbPrefix . 'airports WHERE icao=?',array($_POST['departure']));
+if ($departureAirport == array())
+    errorOut(404,'Departure airport not found');
+$departureAirport = $departureAirport[0];
+$arrivalAirport = $database->fetch('SELECT name FROM ' . dbPrefix . 'airports WHERE icao=?',array($_POST['arrival']));
+if ($arrivalAirport == array())
+    errorOut(404,'Arrival airport not found');
+$arrivalAirport = $arrivalAirport[0];
+$params = array(
+    $dbID,
+    $_POST['number'],
+    $pilotName['firstname'] . ' ' . $pilotName['lastname'],
+    $_POST['aircraft'],
+    $_POST['lat'],
+    $_POST['long'],
+    $_POST['heading'],
+    $_POST['altitude'],
+    $_POST['departure'],
+    $departureAirport['name'],
+    $_POST['arrival'],
+    $arrivalAirport['name'],
+    $_POST['departureTime'],
+    $_POST['flightTime'],
+    $_POST['arrivalTime'],
+    $_POST['route'],
+    $_POST['distance'],
+    $_POST['network'],
+);
+$acarsQuery = $database->execute('INSERT INTO ' . dbPrefix . 'acarsdata (pilotid, flightnum, pilotname, aircraft, lat, lng, heading, alt, gs, depicao, depapt, arricao, arrapt, deptime, timeremaining, arrtime, route, route_details, distremain, phasedetail, online, messagelog, lastupdate, client) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0, ?, ?, ?, ?, ?, ?, ?, ?, "",?, "preflight", ?, "", NOW(), "smartCARS 3")',$params);
+if ($acarsQuery != true)
+    errorOut(500,'Unable to create data instance');
+$instanceID = $database->getLastInsertID();
+if ($database->execute('INSERT INTO ' . dbPrefix . 'bids (bidid, pilotid, routeid, dateadded) VALUES (NULL, ?, ?, NOW())',array($dbID, $scheduleID)) != true)
+    errorOut(500,'Unable to create bid');
 $bidID = $database->getLastInsertID();
-$query = $database->execute('INSERT INTO smartCARS3_CharterFlights (scheduleID, bidID, dbID) VALUES (?, ?, ?)',array($scheduleID, $bidID, $dbID));
-echo(json_encode(array('success'=>$query)));
+$charterQuery = $database->execute('INSERT INTO smartCARS3_CharterFlights (scheduleID, bidID, dbID) VALUES (?, ?, ?)',array($scheduleID, $bidID, $dbID));
+if ($charterQuery != true)
+    errorOut(500,'Unable to create charter flight');
+echo(json_encode(array('success'=>($charterQuery && $acarsQuery),'instanceID'=>$instanceID)));
 ?>
