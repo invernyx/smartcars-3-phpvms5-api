59d1768ea9907acf89f8d5098ca41b5d8fdbcc37
Collin Biedenkapp <cbiedenkapp@tfdidesign.com>
2020-12-10 13:40:20 -0500

    Finished update

--- handlers/phpvms5/flight/update.php
+++ handlers/phpvms5/flight/update.php
@@ -2,53 +2,50 @@
 if(!defined('API'))
     exit;
 
-errorOut(503, 'Work in progress');
+require_once("../core/common/NavData.class.php"); 
+require_once("../core/common/ACARSData.class.php");
 
 assertData(
-    array('instance' => 'number', 'latitude' => 'number', 'longitude' => 'number', 'magneticheading' => 'number', 'trueheading' => 'number', 'altitude' => 'number', 'groundspeed' => 'number', 'phase' => 'string', 'departuretime' => 'string', 'arrivaltime' => 'string', 'distanceremaining' => 'number', 'timeremaining' => 'string'),
-    $_POST
+    $_POST,
+    array('instance' => 'number', 'latitude' => 'string', 'longitude' => 'string', 'magneticheading' => 'number', 'trueheading' => 'number', 'altitude' => 'number', 'groundspeed' => 'number', 'phase' => 'string', 'departuretime' => 'string', 'arrivaltime' => 'string', 'distanceremaining' => 'number', 'timeremaining' => 'string')    
 );
 
 $pilot = $database->fetch('SELECT * FROM ' . dbPrefix . 'pilots WHERE pilotid = ?', array($dbID));
 
 if(!empty($pilot))
-{    
-    //WIP
-    $lat = str_replace(",", ".", $latitude);
-    $lon = str_replace(",", ".", $longitude);
-    
-    $lat = doubleval($lat);
-    $lon = doubleval($lon);
-    
-    if($lon < 0.005 && $lon > -0.005)
-        $lon = 0;
+{       
+    $flight = $database->fetch('SELECT pilotid,flightnum,pilotname,aircraft,lat,lng,heading,alt,gs,depicao,arricao,deptime,arrtime,route,distremain,timeremaining,phasedetail,online,client FROM ' . dbPrefix . 'acarsdata WHERE id = ?', array($_POST['instance']));
+    if(!empty($flight) && !empty($flight[0]))
+    {
+        $flight = $flight[0];
+        $lat = str_replace(",", ".", $_POST['latitude']);
+        $lon = str_replace(",", ".", $_POST['longitude']);
         
-    if($lat < 0.005 && $lat > -0.005)
-        $lat = 0;        
-    
-    $fields = array(
-        'pilotid' =>$dbid,
-        'flightnum' =>$flightnumber,
-        'pilotname' => $pilot['firstname'] . " " . $pilot['lastname'],
-        'aircraft' =>$aircraft,
-        'lat' =>$lat,
-        'lng' =>$lon,
-        'heading' =>$magneticheading,
-        'alt' =>$altitude,
-        'gs' =>$groundspeed,
-        'depicao' =>$departureicao,
-        'arricao' =>$arrivalicao,
-        'deptime' =>$departuretime,
-        'arrtime' =>$arrivaltime,
-        'route' =>$route,
-        'distremain' =>$distanceremaining,
-        'timeremaining' =>$timeremaining,
-        'phasedetail' =>$phases[$phase],
-        'online' => $onlinenetwork,
-        'client' =>'smartCARS',
-    );
-    
-    return(ACARSData::UpdateFlightData($dbid, $fields));
+        $lat = doubleval($lat);
+        $lon = doubleval($lon);
+        
+        if($lon < 0.005 && $lon > -0.005)
+            $lon = 0;
+            
+        if($lat < 0.005 && $lat > -0.005)
+            $lat = 0;  
+
+        $flight['lat'] = $lat;
+        $flight['lng'] = $lon;
+        $flight['alt'] = $_POST['altitude'];
+        $flight['gs'] = $_POST['groundspeed'];
+        $flight['heading'] = $_POST['trueheading'];
+        $flight['phasedetail'] = $_POST['phase'];
+        $flight['deptime'] = $_POST['departuretime'];
+        $flight['arrtime'] = $_POST['arrivaltime'];
+        $flight['distremain'] = $_POST['distanceremaining'];
+        $flight['timeremaining'] = $_POST['timeremaining'];
+
+        if(!ACARSData::UpdateFlightData($dbID, $flight))
+            errorOut(500, 'ACARSData::UpdateFlightData failed');    
+    }
+    else
+        errorOut(404, 'Flight instance not found');
 }
 else
     errorOut(404, 'Pilot data not found');
