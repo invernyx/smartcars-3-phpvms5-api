ecf49ad7e437a60c5c55637f9f9e86121bed9e9f
GenericNerd <45597752+GenericNerd@users.noreply.github.com>
2020-12-15 14:50:34 +0000

    Added PIREP filing

---
+++ handlers/phpvms5/flight/finish.php
@@ -0,0 +1,72 @@
+<?php
+if(!defined('API'))
+    exit;
+
+require_once('../core/common/NavData.class.php');
+require_once('../core/common/ACARSData.class.php');
+
+assertData(
+    $_POST,
+    array('instance'=>'number','bidID'=>'number','load'=>'number','flightTime'=>'string','landingRate'=>'number','fuelUsed'=>'number','log'=>'string')    
+);
+
+$bidCheck = $database->fetch('SELECT bidid FROM ' . dbPrefix . 'bids WHERE bidid=?',array($_POST['bidID']));
+if ($bidCheck == array())
+    errorOut(404,'Bid not found');
+    
+$flight = $database->fetch('SELECT * FROM ' . dbPrefix . 'acarsdata WHERE id=?',array($_POST['instance']));
+if ($flight == null)
+    errorOut(500, 'Unable to locate flight');
+
+$flight = $flight[0];
+if (isset($_POST['code']))
+    $flight['code'] = $_POST['code'];
+else
+    $flight['code'] = 'SCC';
+
+$data = array(
+    'pilotid' => $dbID,
+    'code' => $flight['code'],
+    'flightnum' => $flight['flightnum'],
+    'depicao' => $flight['depicao'],
+    'arricao' => $flight['arricao'],
+    'route' => $flight['route'],
+    'aircraft' => $flight['aircraft'],
+    'load' => $_POST['load'],
+    'flighttime' => $_POST['flightTime'],
+    'landingrate' => $_POST['landingRate'],
+    'submitdate' => date('Y-m-d H:i:s'),
+    'fuelused'=> $_POST['fuelUsed'],
+    'source'=>'smartCARS 3',
+    'log' => $_POST['log']
+);
+if (isset($_POST['comments']))
+    $data['comment'] = $_POST['comments'];
+else
+    $data['comment'] = '';
+
+$return = ACARSData::FilePirep($dbID, $data);
+
+if ($return == false)
+    errorOut(500,'Unable to file PIREP');
+
+$success = true;
+$query = $database->fetch('SELECT * FROM smartCARS3_CharterFlights WHERE bidID=? AND dbID=?',array($_POST['bidID'],$dbID));
+if ($query[0]['scheduleID'] != '') {
+    $database->execute('DELETE FROM smartCARS3_CharterFlights WHERE bidID=? AND dbID=?',array($_POST['bidID'],$dbID));
+    $database->execute('DELETE FROM ' . dbPrefix . 'schedules WHERE id=?',array($query['scheduleID']));
+}
+if (!$database->execute('UPDATE ' . dbPrefix . 'pilots SET retired=0 WHERE pilotid=?',array($dbID)))
+    $success = 'Updating pilot';
+
+if (!$database->execute('UPDATE ' . dbPrefix . 'acarsdata SET gs=0, distremain=0, timeremaining="0:00", phasedetail="Arrived",arrtime=CURRENT_TIMESTAMP WHERE pilotid=?',array($dbID)))
+    $success = 'Updating ACARS data';
+
+if (!$database->execute('DELETE FROM ' . dbPrefix . 'bids WHERE pilotid=? AND bidid=?',array($dbID, $_POST['bidID'])))
+    $success = 'Removing pilot bid';
+
+if ($success != true)
+    errorOut(500, $success . ' failed');
+else
+    echo(json_encode(array('success'=>true)));
+?>
