7cf358fdd9b6cfbdeba6941240ad19b6880da071
GenericNerd <genericnerd.yt@gmail.com>
2021-02-03 17:00:13 +0000

    Removed PIREPs as system is not handled by this module

--- handlers/phpvms5/pilot.php
+++ handlers/phpvms5/pilot.php
@@ -6,6 +6,4 @@ if ($request[1] == 'statistics')
     require_once('pilots/statistics.php');
 else if ($request[1] == 'login')
     require_once('pilots/login.php');
-else if ($request[1] == 'pireps')
-    require_once('pilots/pireps.php');
 ?>
