ecb08301185a5f93f31dba180a51b7d9c52dc2f3
Fabio Almeida <me@fabioalmeida.dev>
2022-01-10 17:27:23 +0000

    Changed minimum rank endpoint to be string

--- 0.2.0/handlers/phpvms5/data/aircraft.php
+++ 0.2.0/handlers/phpvms5/data/aircraft.php
@@ -1,4 +1,27 @@
 <?php
 // Correction will be required to make minimum rank a string instead of int
-echo(json_encode($database->fetch('SELECT id,icao as code,fullname as name,cruise as serviceCeiling,registration,maxpax as maximumPassengers,maxcargo as maximumCargo,minrank as minimumRank FROM ' . dbPrefix . 'aircraft WHERE enabled = 1')));
+$aircrafts = $database->fetch('SELECT id,icao as code,fullname as name,cruise as serviceCeiling,registration,maxpax as maximumPassengers,maxcargo as maximumCargo,minrank as minimumRank FROM ' . dbPrefix . 'aircraft WHERE enabled = 1');
+$ranks = $database->fetch('SELECT rankid, rank FROM ' . dbPrefix . 'ranks');
+$i = 0;
+foreach($aircrafts as $aircraft)
+{
+    switch($aircraft['minimumRank'])
+    {
+        case 0:
+            $aircrafts[$i]['minimumRank'] = 'N/A';
+            break;
+        default:
+            foreach($ranks as $rank)
+            {
+                if($rank['rankid'] == $aircraft['minimumRank'])
+                {
+                    $aircrafts[$i]['minimumRank'] = $rank['rank'];
+                    break;
+                }
+            }
+            break;
+    }
+    $i++;
+}
+echo(json_encode($aircrafts));
 ?>
