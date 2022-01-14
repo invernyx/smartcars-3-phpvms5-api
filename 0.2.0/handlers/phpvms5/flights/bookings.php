840d279078500ffaf20d92957e1b331670cad1c7
Fabio Almeida <me@fabioalmeida.dev>
2022-01-14 13:29:20 +0000

    Completed information and bookings endpoints

--- 0.2.0/handlers/phpvms5/flights/bookings.php
+++ 0.2.0/handlers/phpvms5/flights/bookings.php
@@ -1,3 +1,9 @@
 <?php
-
+$bids = $database->fetch('SELECT bidid FROM ' . dbPrefix . 'bids');
+$bookings = array();
+foreach($bids as $bid)
+{
+    array_push($bookings, $bid['bidid']);
+}
+echo(json_encode(array('bookings' => $bookings)));
 ?>
