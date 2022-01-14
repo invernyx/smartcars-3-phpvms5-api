3a60e4953b7778a79efac51b5bba8b4c772271cb
Fabio Almeida <me@fabioalmeida.dev>
2022-01-14 13:31:04 +0000

    Changed GET to POST parameters

--- 0.2.0/handlers/phpvms5/flights/book.php
+++ 0.2.0/handlers/phpvms5/flights/book.php
@@ -4,12 +4,12 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST')
     error(405, 'POST request method expected, received a ' . $_SERVER['REQUEST_METHOD'] . ' request instead.');
     exit;
 }
-if($_GET['flightID'] === null)
+if($_POST['flightID'] === null)
 {
     error(400, 'flightID is a required field (type `string`)');
     exit;
 }
-assertData($_GET, array('flightID' => 'int'));
+assertData($_POST, array('flightID' => 'int'));
 
 $bids = $database->fetch('SELECT pilotid FROM ' . dbPrefix . 'bids WHERE pilotid=?', array($pilotID));
 if($bids !== array())
@@ -17,7 +17,7 @@ if($bids !== array())
     error(429, 'This pilot already has an existing booking');
     exit;
 }
-$schedule = $database->fetch('SELECT id FROM ' . dbPrefix . 'schedules WHERE id=?', array($_GET['flightID']));
+$schedule = $database->fetch('SELECT id FROM ' . dbPrefix . 'schedules WHERE id=?', array($_POST['flightID']));
 if($schedule === array())
 {
     error(404, 'A flight with this ID could not be found');
@@ -30,7 +30,7 @@ if($bids !== array())
     exit;
 }
 // Rank/aircraft restriction applied here
-$database->execute('INSERT INTO ' . dbPrefix . 'bids (pilotid, routeid, dateadded) VALUES (?, ?, NOW())', array($pilotID, $_GET['flightID']));
+$database->execute('INSERT INTO ' . dbPrefix . 'bids (pilotid, routeid, dateadded) VALUES (?, ?, NOW())', array($pilotID, $_POST['flightID']));
 
 echo(json_encode(array('bidID'=>$database->getLastInsertID('bidid'))));
 ?>
