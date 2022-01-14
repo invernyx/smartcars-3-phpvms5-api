63113b70f4a5bbe6210f617ff50efc0ca1b87b6d
Fabio Almeida <me@fabioalmeida.dev>
2022-01-14 13:04:29 +0000

    Implemented book flight endpoint (missing aircraft restriction)

--- 0.2.0/handlers/phpvms5/flights/book.php
+++ 0.2.0/handlers/phpvms5/flights/book.php
@@ -1,3 +1,36 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] !== 'POST')
+{
+    error(405, 'POST request method expected, received a ' . $_SERVER['REQUEST_METHOD'] . ' request instead.');
+    exit;
+}
+if($_GET['flightID'] === null)
+{
+    error(400, 'flightID is a required field (type `string`)');
+    exit;
+}
+assertData($_GET, array('flightID' => 'int'));
 
+$bids = $database->fetch('SELECT pilotid FROM ' . dbPrefix . 'bids WHERE pilotid=?', array($pilotID));
+if($bids !== array())
+{
+    error(429, 'This pilot already has an existing booking');
+    exit;
+}
+$schedule = $database->fetch('SELECT id FROM ' . dbPrefix . 'schedules WHERE id=?', array($_GET['flightID']));
+if($schedule === array())
+{
+    error(404, 'A flight with this ID could not be found');
+    exit;
+}
+$bids = $database->fetch('SELECT bidid FROM ' . dbPrefix . 'bids WHERE routeid=?', array($schedule['id']));
+if($bids !== array())
+{
+    error(409, 'A different pilot has already booked this flight');
+    exit;
+}
+// Rank/aircraft restriction applied here
+$database->execute('INSERT INTO ' . dbPrefix . 'bids (pilotid, routeid, dateadded) VALUES (?, ?, NOW())', array($pilotID, $_GET['flightID']));
+
+echo(json_encode(array('bidID'=>$database->getLastInsertID('bidid'))));
 ?>
