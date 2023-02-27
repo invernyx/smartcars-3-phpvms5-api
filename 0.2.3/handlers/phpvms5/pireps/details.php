16e7f887302ac35a2bfea306bda1bd9bdc37c124
Fabio Almeida <genericnerd.yt@gmail.com>
2023-02-27 16:46:14 +0000

    Removed manual data validity check, explode on \n

--- 0.2.3/handlers/phpvms5/pireps/details.php
+++ 0.2.3/handlers/phpvms5/pireps/details.php
@@ -1,9 +1,4 @@
 <?php
-if($_GET['id'] === null)
-{
-    error(400, 'PIREP ID is a required parameter (type `int`)');
-    exit;
-}
 assertData($_GET, array('id'=>'int'));
 
 $pirep = $database->fetch('SELECT log as flightLog FROM ' . dbPrefix . 'pireps WHERE pirepid=? AND pilotid=?', array($_GET['id'], $pilotID));
@@ -14,5 +9,6 @@ if($pirep === array())
 }
 $pirep = $pirep[0];
 $pirep['flightLog'] = explode('*', $pirep['flightLog']);
+$pirep['flightLog'] = explode('\n', $pirep['flightLog']);
 echo(json_encode($pirep));
 ?>
