a42d469fd0cd320d132cc946aae9374d15cf70bf
Fabio Almeida <me@fabioalmeida.dev>
2023-03-08 23:22:03 +0000

    Added POST check

--- 0.2.3/handlers/phpvms7/flights/complete.php
+++ 0.2.3/handlers/phpvms7/flights/complete.php
@@ -1,3 +1,7 @@
 <?php
-
+if($_SERVER['REQUEST_METHOD'] !== 'POST')
+{
+    error(405, 'POST request method expected, received a ' . $_SERVER['REQUEST_METHOD'] . ' request instead.');
+    exit;
+}
 ?>
