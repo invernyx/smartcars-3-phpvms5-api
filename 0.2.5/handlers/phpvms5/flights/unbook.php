a255c93a906764c70f59a4dba0073d26e484f3c4
Fabio Almeida <me@fabioalmeida.dev>
2023-04-06 21:20:45 +0100

    Remove flight schedule if flight is charter

--- 0.2.5/handlers/phpvms5/flights/unbook.php
+++ 0.2.5/handlers/phpvms5/flights/unbook.php
@@ -5,8 +5,15 @@ if($_SERVER['REQUEST_METHOD'] !== 'POST')
     exit;
 }
 assertData($_POST, array('bidID'=>'int'));
-if($database->fetch('SELECT bidid FROM ' . dbPrefix . 'bids WHERE pilotid=? AND bidid=?', array($pilotID, $_POST['bidID'])) !== array())
+
+$bid = $database->fetch('SELECT routeid FROM ' . dbPrefix . 'bids WHERE pilotid=? AND bidid=?', array($pilotID, $_POST['bidID']));
+if($bid !== array())
 {
+    $flight = $database->fetch('SELECT id FROM ' . dbPrefix . 'schedules WHERE id=? AND enabled=0 AND notes="smartCARS Charter Flight"');
+    if($flight !== array())
+    {
+        $database->execute('DELETE FROM ' . dbPrefix . 'schedules WHERE id=?', array($flight['id']));
+    }
     $database->execute('DELETE FROM ' . dbPrefix . 'bids WHERE pilotid=? AND bidid=?', array($pilotID, $_POST['bidID']));
 }
 else
