c493bc498cf1463a678bc95b8b170d2f05e8ff3c
Jordan Havard <sirj.havard@gmail.com>
2023-04-25 10:10:08 +1000

    Fetch latest news instead of first

--- 0.3.1/handlers/phpvms5/data/news.php
+++ 0.3.1/handlers/phpvms5/data/news.php
@@ -1,5 +1,5 @@
 <?php
-$news = $database->fetch('SELECT subject as title, body, postdate as postedAt, postedby as postedBy FROM ' . dbPrefix . 'news LIMIT 1');
+$news = $database->fetch('SELECT subject as title, body, postdate as postedAt, postedby as postedBy FROM ' . dbPrefix . 'news ORDER BY postedAt DESC LIMIT 1');
 if($news !== array()) {
     $news = $news[0];
     echo(json_encode($news));
