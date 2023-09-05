171b34ac4e4451f995703e56f175de4c3d618d87
Thiên Ân Lacape <thienan.lacape42@gmail.com>
2023-09-05 23:09:43 +0200

    chore: remove comments

--- 0.3.6/handlers/phpvms7/environment.php
+++ 0.3.6/handlers/phpvms7/environment.php
@@ -6,9 +6,8 @@ $envFilepath = __DIR__ . "/../../../../../.env";
 
 if (is_file($envFilepath)) {
     $file = new \SplFileObject($envFilepath);
-    // Loop until we reach the end of the file.
+
     while (false === $file->eof()) {
-        // Get the current line value, trim it and save by putenv.
         $line = trim($file->fgets());
 
         if (strpos($line, '#') === 0) {
